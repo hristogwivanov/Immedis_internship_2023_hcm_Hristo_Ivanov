07253dba244b19d8acc763d0e6ce22fa516be850
Hristo Ivanov <gw.hivanov@gmail.com>
2023-10-16 15:19:12 +0300

    Dependencies installed

---
+++ routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
+const hcmController = require('./controllers/hcmController');
+
+router.use(homeController);
+router.use(authController);
+router.use('/HCM', hcmController);
+router.all('*', (req, res) => {
+    res.render('home/404')
+});
+
+module.exports = router; 
