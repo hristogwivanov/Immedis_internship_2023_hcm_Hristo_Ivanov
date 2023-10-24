7ab9c109e6287bb32fda5ac631a75d389a286b77
Hristo Ivanov <gw.hivanov@gmail.com>
2023-10-24 15:00:30 +0300

    HBS removed from server
    More hbs pieces removed from the server, now it is starting successfully at port 5000

--- server/index.js
+++ server/index.js
@@ -1,20 +1,11 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const cookieParser = require('cookie-parser');
+const app = express();
 const mongoose = require('mongoose');
-
 const routes = require('./routes');
+const cookieParser = require('cookie-parser');
 const  { authentication } = require('./middlewares/authMiddleware');
 
-const app = express();
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
-
-app.set('view engine', 'hbs');
-
-app.use('/static', express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(authentication);
