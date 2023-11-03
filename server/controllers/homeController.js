c47ca60b97328cc577ab51a26c9ac28652d5a829
Hristo Ivanov <gw.hivanov@gmail.com>
2023-11-03 18:00:13 +0200

    HTTP headers sent error fixed

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -2,9 +2,8 @@ const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 
 router.get('/', (req, res) => {
-    // if(isAuth) res.render('home');
-    // else res.render('auth/login');
-    res.render('auth/login')
+    if(isAuth) res.render('home/home');
+    else res.render('auth/login');
 });
 
 module.exports = router;
