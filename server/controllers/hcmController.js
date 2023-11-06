b4c61eae78752965f1f0afc7ad52ffadcfea4aea
Hristo Ivanov <gw.hivanov@gmail.com>
2023-11-06 16:48:56 +0200

    all users page created

--- server/controllers/hcmController.js
+++ server/controllers/hcmController.js
@@ -4,5 +4,10 @@ const { isAuth } = require('../middlewares/authMiddleware');
 const hcmService = require('../services/hcmService');
 const { getErrorMessage } = require('../utils/errorUtils');
 
+router.get('/allusers', async (req, res) => {
+
+
+    res.render('HCM/allusers', {})
+})
 
 module.exports = router; 
