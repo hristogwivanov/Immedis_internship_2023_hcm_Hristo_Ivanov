cf76f45dfcd815ff655a6140b122faa881695c02
Hristo Ivanov <gw.hivanov@gmail.com>
2023-11-07 15:15:11 +0200

    edit user function implemented on the server

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -4,22 +4,40 @@ const { isAuth } = require('../middlewares/authMiddleware');
 const usersService = require('../services/usersService');
 const { getErrorMessage } = require('../utils/errorUtils');
 
-router.get('/allusers', async (req, res) => {
+router.get('/allusers', isAuth, async (req, res) => {
     currentUserType = req.user?.type;
-    if(currentUserType === "admin"){
+    if (currentUserType === "admin") {
         const users = await usersService.getAllUsers();
         res.render('users/allusers', { users })
     }
-    else {res.render('home/unauthorized', {});}
+    else { res.render('home/unauthorized'); }
 
 })
-router.get('/:userId/edit', async (req, res) => {
-    currentUserType = req.user?.type;
-    if(currentUserType === "admin"){
+router.get('/:userId/edit', isAuth, async (req, res) => {
+    const currentUserType = req.user?.type;
+    if (currentUserType === "admin") {
         const user = await usersService.getOne(req.params.userId);
-        res.render('users/edit', { user });
+        isHR = (user.type === "hr");
+        if (user.type === "admin") {
+            const users = await usersService.getAllUsers();
+            return res.status(400).render('users/allusers', { error: "Admin details cannot be changed from the UI", users })
+        }
+        res.render('users/edit', { user, isHR });
     }
-    else {res.render('home/unauthorized', {});}
+    else { res.render('home/unauthorized'); }
 });
+
+router.post('/:userId/edit', isAuth, async (req, res) => {
+    const userData = req.body;
+    try {
+        await usersService.editUser(req.params.userId, userData);
+        const users = await usersService.getAllUsers();
+        res.redirect('/users/allusers');
+    }catch(error){
+        res.status(400).render('/:userId/edit', {error: getErrorMessage(error) })
+    }
+
+});
+
 
 module.exports = router; 
